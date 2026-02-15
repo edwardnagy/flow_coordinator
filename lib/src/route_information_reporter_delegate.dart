2947f9901c7f051d2281eaeceb07a64dd31857d9
Edward Nagy <52851669+edwardnagy@users.noreply.github.com>
2026-02-15 12:19:04 +0200

    Improve route information reporting logic and optimize listener notifications

--- lib/src/route_information_reporter_delegate.dart
+++ lib/src/route_information_reporter_delegate.dart
@@ -48,10 +48,11 @@ class RootRouteInformationReporterDelegate
       'Route information reporting task was scheduled but no route information '
       'is pending.',
     );
-    final routeInformation = _pendingRouteInformation!;
+    _reportedRouteInformation = _pendingRouteInformation!;
     _pendingRouteInformation = null;
-    _reportedRouteInformation = routeInformation;
-    notifyListeners();
+    if (hasListeners) {
+      notifyListeners();
+    }
   }
 
   @override
@@ -70,11 +71,7 @@ class RootRouteInformationReporterDelegate
     final isReportingNotScheduled = _pendingRouteInformation == null;
     _pendingRouteInformation = prefixedRouteInformation;
     if (isReportingNotScheduled) {
-      WidgetsBinding.instance.addPostFrameCallback(
-        (_) => _reportRouteInformation(),
-        debugLabel:
-            'RootRouteInformationReporterDelegate.reportRouteInformation',
-      );
+      Future(_reportRouteInformation);
     }
   }
 }
